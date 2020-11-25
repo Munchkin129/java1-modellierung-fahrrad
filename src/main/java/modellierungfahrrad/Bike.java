24b57ad22e89b8e791679b88ec1530838ecb0b9e
Munchkin129 <dirk.hofmann@fh-erfurt.de>
2020-11-25 21:30:34 +0100

    minus Konstruktor minus Get/Set

--- src/main/java/modellierungfahrrad/Bike.java
+++ src/main/java/modellierungfahrrad/Bike.java
@@ -5,6 +5,14 @@ enum typeOfGearSystem {
     HUBGEAR
 }
 
+enum colour {
+    RED,
+    GREEN,
+    YELLOW,
+    WHITE,
+    BLACK,
+    BLUE
+}
 public class Bike {
     private static final int NUMBER_OF_TIRES = 2;
     private static final String EMPTY_OWNER_VALUE = "no owner";
@@ -12,69 +20,14 @@ public class Bike {
 
     private String bikeName;
     private String companyName;
-    private String bikeOwner;
+    private String owner;
     private typeOfGearSystem gearSystem;
     private int numberOfGears;
-    private float bikePrice;
-
-
-    public String getBikeName() {
-        return bikeName;
-    }
-
-    public void setBikeName(String bikeName) {
-        this.bikeName = bikeName;
-    }
-
-    public String getCompanyName() {
-        return companyName;
-    }
-
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName;
-    }
-
-    public String getBikeOwner() {
-        return bikeOwner;
-    }
-
-    public void setBikeOwner(String bikeOwner) {
-        this.bikeOwner = bikeOwner;
-    }
-
-    public typeOfGearSystem getGearSystem() {
-        return gearSystem;
-    }
-
-    public void setGearSystem(typeOfGearSystem gearSystem) {
-        this.gearSystem = gearSystem;
-    }
-
-    public int getNumberOfGears() {
-        return numberOfGears;
-    }
-
-    public void setNumberOfGears(int numberOfGears) {
-        this.numberOfGears = numberOfGears;
-    }
-
-    public float getBikePrice() {
-        return bikePrice;
-    }
-
-    public void setBikePrice(float bikePrice) {
-        this.bikePrice = bikePrice;
-    }
-
-    public Bike(String bikename, String companyName, int numberOfGears, float bikePrice) {
-        this.bikeName = bikename;
-        this.companyName = companyName;
-        this.numberOfGears = numberOfGears;
-        this.bikePrice = bikePrice;
-
-        this.bikeOwner = EMPTY_OWNER_VALUE;
-        this.gearSystem = DEFAULT_TYPE_OF_GEAR_SYSTEM;
-    }
+    private float price;
+    private String bikeDespriction;
+    private String articleNumber;
+    private boolean lightsIncluded;
+    private colour colour;
 
 
 }
--- src/main/java/modellierungfahrrad/Main.java
+++ src/main/java/modellierungfahrrad/Main.java
@@ -6,9 +6,10 @@ public class Main {
         System.out.println( "Hello" );
         Bike TestBike1 = new Bike("Hansi","Texas",21,200);
         String hilfe = TestBike1.getBikeName();
-        System.out.println("Dieses Fahrrad heißt ",
-                hilfe ,
-                " und hat "); // , TestBike1.getNumberOfGears() , " Gänge.");
+        System.out.println("Dieses Fahrrad heißt "+ TestBike1.getBikeName() + " und hat " + TestBike1.getNumberOfGears() + " Gänge und gehört " + TestBike1.getBikeOwner());
+        TestBike1.setNumberOfGears(TestBike1.getNumberOfGears()+1);
+        TestBike1.setBikeOwner("Günther");
+        System.out.println("Dieses Fahrrad heißt "+ TestBike1.getBikeName() + " und hat " + TestBike1.getNumberOfGears() + " Gänge und gehört " + TestBike1.getBikeOwner());
     }
 }
 
